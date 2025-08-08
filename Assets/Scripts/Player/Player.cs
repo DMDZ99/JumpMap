3d55a169c63b28ca6a2f5466e1b8af3cf16081c5
DMDZ99 <ldhdo430@naver.com>
2025-08-08 17:12:10 +0900

    아이템장착구현

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public PlayerController controller;
     public PlayerCondition condition;
 
+    public Equipment equip;
+
     public ItemData itemData;
     public Action addItem;
 
@@ -18,5 +20,6 @@ public class Player : MonoBehaviour
         CharacterManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
         condition = GetComponent<PlayerCondition>();
+        equip = GetComponent<Equipment>();
     }
 }
--- Assets/Scripts/UI/UIInventory.cs
+++ Assets/Scripts/UI/UIInventory.cs
@@ -29,6 +29,8 @@ public class UIInventory : MonoBehaviour
     ItemData selectedItem;
     int selectedItemIndex = 0;
 
+    int curEquipIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -145,9 +147,9 @@ public class UIInventory : MonoBehaviour
     {
         for (int i = 0; i < slots.Length; i++)
         {
-            if (slots[i].item == data && slots[i].quantity < data.maxStackAmount)
+            if (slots[i].item == data && slots[i].quantity < data.maxStackAmount)   // 슬롯아이템이 데이터와 같고 슬롯 개수가 최대개수보다 많으면
             {
-                return slots[i];
+                return slots[i];    // 슬롯 반환
             }
         }
         return null;
@@ -157,22 +159,22 @@ public class UIInventory : MonoBehaviour
     {
         for (int i = 0; i < slots.Length; i++)
         {
-            if (slots[i].item == null)
+            if (slots[i].item == null)  // 아이템슬롯이 null이면
             {
-                return slots[i];
+                return slots[i];        // 슬롯 반환
             }
         }
         return null;
     }
 
-    void ThrowItem(ItemData data)
+    void ThrowItem(ItemData data)   // 아이템버리기
     {
-        Instantiate(data.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360));
+        Instantiate(data.dropPrefab, dropPosition.position, Quaternion.Euler(Vector3.one * Random.value * 360));    // dropPrefab을 생성. dropPosition에, 랜덤회전값으로
     }
 
     public void SelectItem(int index)
     {
-        if (slots[index].item == null) return;
+        if (slots[index].item == null) return;      // 슬롯에 아이템이 널이면 반환
 
         selectedItem = slots[index].item;
         selectedItemIndex = index;
@@ -185,55 +187,87 @@ public class UIInventory : MonoBehaviour
 
         for (int i = 0; i < selectedItem.consumables.Length; i++)
         {
-            selectedStatName.text += selectedItem.consumables[i].type.ToString() + "\n";
-            selectedStatValue.text += selectedItem.consumables[i].value.ToString() + "\n";
+            selectedStatName.text += selectedItem.consumables[i].type.ToString() + "\n";        // 각 선택된스탯이름에 먹을수있는지 타입 추가
+            selectedStatValue.text += selectedItem.consumables[i].value.ToString() + "\n";      // 각 선택된스탯값에 먹는 양 값 추가
         }
 
-        useButton.SetActive(selectedItem.type == ItemType.Consumable);
-        equipButton.SetActive(selectedItem.type == ItemType.Equipable && !slots[index].equipped);
-        unequipButton.SetActive(selectedItem.type == ItemType.Equipable && slots[index].equipped);
-        dropButton.SetActive(true);
+        useButton.SetActive(selectedItem.type == ItemType.Consumable);  // 먹을수있는 타입 : 사용버튼 활성화
+        equipButton.SetActive(selectedItem.type == ItemType.Equipable && !slots[index].equipped);   // 장착가능타입, 장착된상태가 아니면 : 장착버튼 활성화
+        unequipButton.SetActive(selectedItem.type == ItemType.Equipable && slots[index].equipped);  // 장착가능타입, 장착된 상태면 : 해제버튼 활성화
+        dropButton.SetActive(true); // 버리기 버튼 항상 활성화
     }
 
     public void OnUseButton()
     {
-        if(selectedItem.type == ItemType.Consumable)
+        if(selectedItem.type == ItemType.Consumable)                    // 선택된 아이템 타입이 먹을수있는 타입이면
         {
             for( int i = 0; i < selectedItem.consumables.Length; i++)
             {
                 switch (selectedItem.consumables[i].type)
                 {
-                    case ConsumableType.Health:
+                    case ConsumableType.Health:                             // 체력회복타입일때 회복
                         condition.Heal(selectedItem.consumables[i].value);
                         break;
                     case ConsumableType.Hunger:
-                        condition.Eat(selectedItem.consumables[i].value);
+                        condition.Eat(selectedItem.consumables[i].value);   // 먹는타입일때 배고픔회복
                         break;
                 }
             }
 
-            RemoveSelectedItem();
+            RemoveSelectedItem();   // 아이템 제거 (먹으면 제거)
         }
     }
 
-    public void OnDropButton()
+    public void OnDropButton()  // 온드롭버튼 누르면 버리고 선택된아이템제거
     {
         ThrowItem(selectedItem);
         RemoveSelectedItem();
     }
 
-    void RemoveSelectedItem()
+    void RemoveSelectedItem()   // 선택된아이템제거 메서드
     {
-        slots[selectedItemIndex].quantity--;
+        slots[selectedItemIndex].quantity--;    // 개수 1 차감
 
-        if (slots[selectedItemIndex].quantity <= 0)
+        if (slots[selectedItemIndex].quantity <= 0) // 수량이 0이하면
         {
-            selectedItem = null;
-            slots[selectedItemIndex].item = null;
-            selectedItemIndex = -1;
-            ClearSelectedItemWindow();
+            selectedItem = null;                    // 선택된아이템 널값
+            slots[selectedItemIndex].item = null;   // 인덱스 널값
+            selectedItemIndex = -1;                 // 선택 인덱스 -1
+            ClearSelectedItemWindow();              // 선택창 초기화
         }
 
         UpdateUI();
+    }
+
+    public void OnEquipButton()
+    {
+        if (slots[curEquipIndex].equipped)
+        {
+            UnEquip(curEquipIndex);
+        }
+
+        slots[selectedItemIndex].equipped = true;
+        curEquipIndex = selectedItemIndex;
+        CharacterManager.Instance.Player.equip.EquipNew(selectedItem);
+        UpdateUI();
+
+        SelectItem(selectedItemIndex);
+    }
+
+    void UnEquip(int index)
+    {
+        slots[index].equipped = false;
+        CharacterManager.Instance.Player.equip.UnEquip();
+        UpdateUI();
+
+        if(selectedItemIndex == index)
+        {
+            SelectItem(selectedItemIndex);
+        }
+    }
+
+    public void OnUnEquipButton()
+    {
+        UnEquip(selectedItemIndex);
     }
 }
