90cf114479c050d6261aabd17b56b04577ec5fe6
DMDZ99 <ldhdo430@naver.com>
2025-08-11 17:30:21 +0900

    프리팹아이콘정리,스크립트정리1

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IInteractable
+public interface IInteractable      // 상호작용 인터페이스
 {
-    public string GetInteractPrompt();
-    public void OnInteract();
+    public string GetInteractPrompt();  // 상호작용 시 보여줄 문자열
+    public void OnInteract();           // 상호작용 시 호출
 }
 
 public class ItemObject : MonoBehaviour, IInteractable
@@ -14,14 +14,14 @@ public class ItemObject : MonoBehaviour, IInteractable
 
     public string GetInteractPrompt()
     {
-        string str = $"{data.displayName}\n{data.description}";
+        string str = $"{data.displayName}\n{data.description}"; // 아이템이름 -  설명
         return str;
     }
 
     public void OnInteract()
     {
-        CharacterManager.Instance.Player.itemData = data;
-        CharacterManager.Instance.Player.addItem?.Invoke();
+        CharacterManager.Instance.Player.itemData = data;   // 플레이어 아이템 데이터에 데이터 저장
+        CharacterManager.Instance.Player.addItem?.Invoke(); // 플레이어에게 아이템을 추가. null아니면
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Obstacles/Jump.cs
+++ Assets/Scripts/Obstacles/Jump.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float jumpVelocity = 10f;    // 점프 속도 10f
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();    // 충돌한 플레이어 오브젝트에서 Rigidbody 컴포넌트 가져옴
+            if (playerRb != null)
+            {
+                Vector3 velocity = playerRb.velocity;       // 현재 속도 저장
+                velocity.y = jumpVelocity;  // y축 방향으로 점프속도 줌
+                playerRb.velocity = velocity;
+            }
+        }
     }
 }
